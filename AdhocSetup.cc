b55c43f01849cf613d19ab977b70375a143c18e2
EDRappaport <ElliRappaport@gmail.com>
2014-04-25 01:14:59 -0400

    started outline and settings

---
+++ AdhocSetup.cc
@@ -0,0 +1,124 @@
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/config-store-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/olsr-helper.h"
+#include "ns3/ipv4-static-routing-helper.h"
+#include "ns3/ipv4-list-routing-helper.h"
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+NS_LOG_COMPONENT_DEFINE ("myStart");
+
+using namespace ns3;
+
+int int main(int argc, char const *argv[]){
+	std::string phyMode ("DsssRate11Mbps");
+	int numNodes = 25;
+	uint32_t sourceNode = 24;
+	//sink node is MBS node 0
+	double distance = 150; // m
+
+	NodeContainer nc;
+	nc.create(numNodes);
+
+	WifiHelper wifi;
+	wifi.EnableLogComponents ();
+	wifi.SetStandard (WIFI_PHY_STANDARD_80211b);
+	wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
+                                "DataMode",StringValue (phyMode),
+                                "ControlMode",StringValue (phyMode));
+	
+
+	YansWifiChannelHelper wifiChannel;
+	wifiChannel.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
+	//wifiChannel.AddPropagationLoss ("ns3::FriisPropagationLossModel");
+	//wifiChannel.AddPropagationLoss ("ns3::LogDistancePropagationLossModel");
+	//wifiChannel.AddPropagationLoss ("ns3::FixedRssLossModel");
+	wifiChannel.AddPropagationLoss ("ns3::BuildingsPropagationLossModel");
+	//wifiChannel.SetNext("ns3::LogDistancePropagationLossModel");
+
+
+	YansWifiPhyHelper wifiPhy =  YansWifiPhyHelper::Default ();
+	wifiPhy.SetPcapDataLinkType (YansWifiPhyHelper::DLT_IEEE802_11);
+	
+	//all parameters below are set to the defaults:
+	wifiPhy.Set ("TxPowerStart", DoubleValue(16.0206));
+	wifiPhy.Set ("TxPowerEnd", DoubleValue(16.0206));
+	wifiPhy.Set ("TxPowerLevels", UintegerValue(1));
+	wifiPhy.Set ("TxGain", DoubleValue(1));
+	wifiPhy.Set ("RxGain", DoubleValue(1));
+	wifiPhy.Set ("EnergyDetectionThreshold", DoubleValue(-96));
+	wifiPhy.Set ("CcaMode1Threshold", DoubleValue(-99));
+
+	wifiPhy.SetChannel (wifiChannel.Create ());
+
+
+	NqosWifiMacHelper wifiMac = NqosWifiMacHelper::Default ();
+	wifiMac.SetType ("ns3::AdhocWifiMac");
+
+
+	NetDeviceContainer devices;
+	devices = wifi.Install (wifiPhy, wifiMac, nc);
+
+
+	MobilityHelper mobilityMBS;
+	Ptr<ListPositionAllocator> positionAlloc = CreateObject <ListPositionAllocator>();
+	positionAlloc->Add(Vector(0, 0, 75)); //node 0 should be center and 75m high
+	mobilityMBS.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+	mobilityMBS.Install(nc.Get(0));
+
+	MobilityHelper mobilityUsers;
+	mobilityUsers.SetPositionAllocator ("ns3::GridPositionAllocator",
+	                             "MinX", DoubleValue (-750.0),
+	                             "MinY", DoubleValue (-750.0),
+	                             "DeltaX", DoubleValue (distance),
+	                             "DeltaY", DoubleValue (distance),
+	                             "GridWidth", UintegerValue (5),
+	                             "LayoutType", StringValue ("RowFirst"));
+	mobilityUsers.SetMobilityModel("ns3::RandomWalk2dMobilityModel",
+		"Bounds", RectangleValue(Rectangle(-1000, 1000, -1000, 1000)));
+	for (int i = 1; i < numNodes; i++)
+		mobilityUsers.Install(nc.Get(i));
+
+	
+
+	OlsrHelper olsr;
+
+	Ipv4ListRoutingHelper list;
+	list.Add (olsr, 10);
+
+	InternetStackHelper internet;
+	internet.SetRoutingHelper (list);
+	internet.Install (nc);
+
+	Ipv4AddressHelper ipv4;
+	NS_LOG_INFO ("Assign IP Addresses.");
+	ipv4.SetBase ("10.1.1.0", "255.255.255.0");
+	Ipv4InterfaceContainer ifcont = ipv4.Assign (devices);
+
+
+	//
+	// Application:
+	//
+
+	TypeId tid = TypeId::LookupByName ("ns3::UdpSocketFactory");
+	Ptr<Socket> recvSink = Socket::CreateSocket (nc.Get(0), tid);
+	InetSocketAddress local = InetSocketAddress (Ipv4Address::GetAny (), 80);
+	recvSink->Bind (local);
+	//recvSink->SetRecvCallback (MakeCallback (&ReceivePacket));
+
+	Ptr<Socket> source = Socket::CreateSocket (nc.Get (sourceNode), tid);
+	InetSocketAddress remote = InetSocketAddress (ifcont.GetAddress (sinkNode, 0), 80);
+	source->Connect (remote);
+
+
+
+
+	return 0;
+}
